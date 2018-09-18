2641b682d1058ebe2c65a63e60c7b9d96b61ea50
Manvijay <manvijay@buffalo.edu>
2018-09-18 05:46:33 -0400

    Add files via upload

---
+++ jsMain.js
@@ -0,0 +1,47 @@
+/*
+function popUp() {
+    var x = document.getElementById("mc_embed_signup");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}*/
+
+// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the button that opens the modal
+var btn1 = document.getElementById("earlyAccess");
+var btn2 = document.getElementById("preOrder");
+var btn3 = document.getElementById("preOrderBigButton");
+var btn4 = document.getElementById("driveSafe");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal
+btn1.onclick = function() {
+    modal.style.display = "block";
+}
+btn2.onclick = function() {
+    modal.style.display = "block";
+}
+btn3.onclick = function() {
+    modal.style.display = "block";
+}
+btn4.onclick = function() {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
